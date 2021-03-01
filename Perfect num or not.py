4e93501096455124058f046cad40c9272474219f
Rohit Waghmare <76784005+therohit1@users.noreply.github.com>
2021-03-01 11:59:20 +0530

    Delete Perfect num or not.py

--- Perfect num or not.py
+++
@@ -1,9 +0,0 @@
-n=int(input("Enter no:"))
-sum=0
-for i in range(1,n):
-    if n%i==0:
-        sum=sum+i
-        print(n," is Perfect no !")
-        exit(0)
-    else:
-        print("not perfect")
