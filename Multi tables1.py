0118ebcd10a03ddf19f064d7d32f9ca4d967694f
Rohit Waghmare <76784005+therohit1@users.noreply.github.com>
2021-03-01 11:55:10 +0530

    Delete Multi tables1.py

--- Multi tables1.py
+++
@@ -1,9 +0,0 @@
-num=int(input("Enter your no:"))
-rows=1
-while rows<=10:# 1 to 10 rows
-    column=1
-    while column<=num:
-        print(rows*column,end="\t")
-        column+=1
-    print("\t")
-    rows+=1
